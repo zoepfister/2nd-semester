fe4f169115e2c63a212708ed3e25eaa21aa2ae60
Clemens Pfister <pfistecl@me.com>
2017-03-28 21:57:02 +0200

    removed unused vars & added comments

--- Betriebssysteme/Task 1/my_back_up.sh
+++ Betriebssysteme/Task 1/my_back_up.sh
@@ -9,37 +9,43 @@ printf "%b" "usage: myscript file1 op file2\n" >&2
 exit 1
 fi
 
+# If there's no directory with the argument name,
+# create one
 if [[ ! -d "$@" ]]; then
 	mkdir "$@"
 fi
 
+# for each file in current directory
 for file in *
 do	
-found_item="false"
-
+	# for each file in the specified backup dir
 	for backup in $@/*; do
 		# printf "$file, $backup\n"
-
+		
+		# if a file with the name file exists in backup dir
 		if [[ -a $@/$file ]]; then
-			if [ "$file" -nt "$backup" ]; then
+			# and was updated
+			if [ "$file" -nt "$@/$file" ]; then
+				# and is NOT the backup directory
 				if [[ "$file" != $@ ]]; then
+					# update the file
 					cp -R "$file" "$@"
 					printf "'$file' has been updated within $@.\n"
-					found_item="true"
-					# echo $found_item
+					# so the new file gets updated to current modification time
+					touch "$@/$file"
 					break
 				fi
 			fi
 		fi
+		# if there was no item found 
 		if [[ ! -a $@/$file ]]; then
+			# and is NOT the backup directory
 			if [[ "$file" != $@ ]]; then
+				# add the file
 				cp -R "$file" "$@"
 				printf "'$file' has been added to $@.\n"
-				found_item="false"
+			fi
 		fi
-	fi
-		#echo $found_item
 	done
-	
 done
 
